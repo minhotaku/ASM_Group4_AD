451d0ecfe932035e1cfd6870844363abda944496
LeBaoMy <lebaomy584@gmail.com>
2025-03-29 16:32:40 +0700

    Them ham dinh dang million vnd

--- app/src/main/java/com/project/cem/utils/VndCurrencyFormatter.java
+++ app/src/main/java/com/project/cem/utils/VndCurrencyFormatter.java
@@ -6,23 +6,48 @@ import java.util.Locale;
 
 public class VndCurrencyFormatter {
     private final DecimalFormat formatter;
+    private final DecimalFormat millionFormatter;
 
     public VndCurrencyFormatter() {
         DecimalFormatSymbols symbols = new DecimalFormatSymbols(new Locale("vi", "VN"));
         symbols.setGroupingSeparator('.');
+        symbols.setDecimalSeparator(',');
 
         formatter = new DecimalFormat("#,###", symbols);
         formatter.setGroupingSize(3);
         formatter.setMinimumFractionDigits(0);
         formatter.setMaximumFractionDigits(0);
+
+        millionFormatter = new DecimalFormat("#,##0.00", symbols);
+        millionFormatter.setGroupingSize(3);
     }
 
     public String format(double amount) {
         return formatter.format(amount) + " VNĐ";
     }
 
-    // Hàm format nhưng không thêm " VNĐ" ở EditText
     public String formatForEditText(double amount) {
         return formatter.format(amount);
+    }
+
+
+    public String formatInMillions(double amount) {
+        double millions = amount / 1_000_000;
+        return millionFormatter.format(millions) + "M";
+    }
+
+
+    public String formatInMillionsForChart(double amount) {
+        double millions = amount / 1_000_000;
+        return millionFormatter.format(millions);
+    }
+
+
+    public String formatAutoUnit(double amount) {
+        if (amount >= 1_000_000) {
+            return formatInMillions(amount);
+        } else {
+            return format(amount);
+        }
     }
 }
